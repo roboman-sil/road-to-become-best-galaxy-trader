a4df61bc6effdd0f8ca2246b2acd5fd101a03328
Sebastian Sim <s.sim97@hotmail.com>
2019-11-20 18:44:51 +0800

    Traderbot starting input framework

--- TraderBot/TraderBot.js
+++ TraderBot/TraderBot.js
@@ -1,34 +1,42 @@
 const readline = require('readline');
 
 class TraderBot {
-  run() {
-    const rl = readline.createInterface({
+  constructor() {
+    this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
-
-    this.question(rl);
   }
 
-  question(rl) {
-    rl.question('What do you think of Node.js? ', answer => {
-      // TODO: Log the answer in a database
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+  run() {
+    console.log('programme initiating...');
+    console.log(
+      `Trading Bot has completed booting. If you wish to exit from the conversation at any point in time, just enter 'close'`,
+    );
+    console.log('Greetings Master, what is new on the galaxy market today?');
 
-      if (answer === 'close') {
-        rl.close();
-      }
-      this.input(rl);
-    });
+    this.rl.question('', answer => this.questionResponse(answer, this.rl));
   }
 
-  input(rl) {
-    rl.on('line', input => {
-      console.log(`Received: ${input}`);
+  questionResponse(answer, rl) {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+    if (answer === 'close') {
+      console.log(
+        'Master, I hope to see you again soon. I hope my assistance has made you wealthier.',
+      );
+      return rl.close();
+    }
 
-      this.input(rl);
-    });
+    rl.question('', answer => this.questionResponse(answer, this.rl));
   }
+
+  // input(rl) {
+  //   rl.on('line', input => {
+  //     console.log(`Received: ${input}`);
+
+  //     this.input(rl);
+  //   });
+  // }
 }
 
 module.exports = TraderBot;
