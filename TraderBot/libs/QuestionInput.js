d91f70879222b4b7787a456ec0ed21387223a963
Sebastian Sim <s.sim97@hotmail.com>
2019-11-20 21:14:12 +0800

    QuestionInput class is able to check if the input is a valid QuestionInput

--- TraderBot/libs/QuestionInput.js
+++ TraderBot/libs/QuestionInput.js
@@ -1,3 +1,15 @@
-class QuestionInput {}
+class QuestionInput {
+  isAValidQuestion(input) {
+    const lowercasedInput = input.toLowerCase();
+
+    if (
+      lowercasedInput.includes('how many') ||
+      lowercasedInput.includes('how much')
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
 
 module.exports = QuestionInput;
--- test/libs/QuestionInput.test.js
+++ test/libs/QuestionInput.test.js
@@ -1,3 +1,31 @@
 const QuestionInput = require('../../TraderBot/libs/QuestionInput');
 
-describe('QuestionInput', () => {});
+describe('QuestionInput', () => {
+  it('should be able to detect if entered statement is a question', () => {
+    const questionInput = new QuestionInput();
+
+    const result = questionInput.isAValidQuestion(
+      'how much is pish tegj glob glob ',
+    );
+    const result2 = questionInput.isAValidQuestion(
+      'how many Credits is glob prok Silver ?',
+    );
+
+    expect(result).toBe(true);
+    expect(result2).toBe(true);
+  });
+
+  it('should be able to detect if entered statement is not a question', () => {
+    const questionInput = new QuestionInput();
+
+    const result = questionInput.isAValidQuestion(
+      'what is pish tegj glob glob ',
+    );
+    const result2 = questionInput.isAValidQuestion(
+      'is there glob prok Silver left?',
+    );
+
+    expect(result).toBe(false);
+    expect(result2).toBe(false);
+  });
+});
