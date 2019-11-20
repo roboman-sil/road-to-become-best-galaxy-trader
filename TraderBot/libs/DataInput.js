fea078ddd15f132f39aebb01078205f82582c0ff
Sebastian Sim <s.sim97@hotmail.com>
2019-11-20 21:13:57 +0800

    DataInput class is able to check if the input is a valid DataInput

--- TraderBot/libs/DataInput.js
+++ TraderBot/libs/DataInput.js
@@ -1,3 +1,37 @@
-class DataInput {}
+const romanNumericTable = require('../data/RomanNumericTable.json');
+
+class DataInput {
+  isAValidDataInput(input) {
+    const romanNumeralList = Object.keys(romanNumericTable);
+
+    if (this.inputHasRomanNumeral(input)) {
+      for (let romanNumeral of romanNumeralList) {
+        if (input.includes(`is ${romanNumeral}`)) {
+          return true;
+        }
+      }
+    }
+
+    if (/\d/.test(input)) {
+      return true;
+    }
+    return false;
+  }
+
+  inputHasRomanNumeral(input) {
+    const inputWords = input.split(' ');
+
+    const romanNumeralList = Object.keys(romanNumericTable);
+
+    for (let word of inputWords) {
+      for (let romanNumeral of romanNumeralList) {
+        if (word === romanNumeral) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
 
 module.exports = DataInput;
--- test/libs/DataInput.test.js
+++ test/libs/DataInput.test.js
@@ -1,3 +1,44 @@
 const DataInput = require('../../TraderBot/libs/DataInput');
 
-describe('DataInput', () => {});
+describe('DataInput', () => {
+  it('should be able to detect if entered statement is a question', () => {
+    const dataInput = new DataInput();
+
+    const result = dataInput.isAValidDataInput('glob is X');
+    const result2 = dataInput.isAValidDataInput(
+      'glob glob glob Diamond is 5660 Credits',
+    );
+
+    expect(result).toBe(true);
+    expect(result2).toBe(true);
+  });
+
+  it('should be able to detect if entered statement is not a question', () => {
+    const dataInput = new DataInput();
+
+    const result = dataInput.isAValidDataInput('Gob bog asd is in Credits');
+    const result2 = dataInput.isAValidDataInput('Rock not Ding is Diamond');
+
+    expect(result).toBe(false);
+    expect(result2).toBe(false);
+  });
+
+  it('should be able to check input has roman numeral', () => {
+    const dataInput = new DataInput();
+
+    const result = dataInput.inputHasRomanNumeral('Glod is X');
+    const result2 = dataInput.inputHasRomanNumeral('papa is V');
+
+    expect(result).toBe(true);
+    expect(result2).toBe(true);
+  });
+  it('should be able to check input has no roman numeral', () => {
+    const dataInput = new DataInput();
+
+    const result = dataInput.inputHasRomanNumeral('there is an Xios');
+    const result2 = dataInput.inputHasRomanNumeral('Rock not Ding is Diamond');
+
+    expect(result).toBe(false);
+    expect(result2).toBe(false);
+  });
+});
