86c6a620d88bcde104558a943dbf881b5dbd6dac
Sebastian Sim <s.sim97@hotmail.com>
2019-11-20 23:25:07 +0800

    Update DataManager to handle update data tables

--- TraderBot/libs/DataManager.js
+++ TraderBot/libs/DataManager.js
@@ -2,16 +2,23 @@ const fs = require('fs');
 
 class DataManager {
   updateData(tableName, data) {
-    let stringifiedData = JSON.stringify(data);
+    let updatedData = data;
+    if (fs.existsSync(tableName)) {
+      const fileData = fs.readFileSync(tableName);
+      const jsonData = JSON.parse(fileData);
+      updatedData = { ...jsonData, ...data };
+    }
+
+    const stringifiedData = JSON.stringify(updatedData);
     fs.writeFileSync(tableName, stringifiedData);
   }
 
   updateMaterialTable(data) {
-    this.updateData('./MaterialTable.json', data);
+    this.updateData('./TraderBot/data/MaterialTable.json', data);
   }
 
   updateVocabularyTable(data) {
-    this.updateData('./VocabularyTable.json', data);
+    this.updateData('./TraderBot/data/VocabularyTable.json', data);
   }
 }
 
--- test/libs/DataManager.test.js
+++ test/libs/DataManager.test.js
@@ -4,23 +4,25 @@ const DataManager = require('../../TraderBot/libs/DataManager');
 describe('DataManager', () => {
   const dummyTable = './dummyTable.json';
 
-  it('should be able to update table data', () => {
+  it('should be able to update existing table data', () => {
     const dataManager = new DataManager();
     const data = { test: 'test', test1: 'test2' };
 
     dataManager.updateData(dummyTable, data);
+    const newData = { test1: 'test22', test2: 'test3' };
+    dataManager.updateData(dummyTable, newData);
 
     fs.readFile(dummyTable, (err, fileData) => {
       if (err) throw err;
       let jsonData = JSON.parse(fileData);
-      expect(jsonData).toEqual(data);
+      expect(jsonData).toEqual({ ...data, ...newData });
     });
     fs.unlinkSync(dummyTable);
   });
 
   it('should be able to update Material Table data', () => {
     const dataManager = new DataManager();
-    const tableName = './MaterialTable.json';
+    const tableName = './TraderBot/data/MaterialTable.json';
     const data = { test: 'test', test1: 'test2' };
 
     dataManager.updateMaterialTable(data);
@@ -35,7 +37,7 @@ describe('DataManager', () => {
 
   it('should be able to update Vocabulary Table data', () => {
     const dataManager = new DataManager();
-    const tableName = './VocabularyTable.json';
+    const tableName = './TraderBot/data/VocabularyTable.json';
     const data = { test: 'test', test1: 'test2' };
 
     dataManager.updateVocabularyTable(data);
