41544e51d0c622e14674dd22713b280e6fd219c1
Sebastian Sim <s.sim97@hotmail.com>
2019-11-24 03:10:07 +0800

    SetupData file

---
+++ test/setup/setupData.js
@@ -0,0 +1,20 @@
+jest.mock('../../TraderBot/data/romanNumericTable.json', () => ({
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+}));
+jest.mock('../../TraderBot/data/materialTable.json', () => ({
+  Gold: 200,
+  Silver: 17,
+  Iron: 195.5,
+}));
+jest.mock('../../TraderBot/data/vocabularyTable.json', () => ({
+  glob: 'I',
+  prok: 'V',
+  pish: 'X',
+  tegj: 'L',
+}));
